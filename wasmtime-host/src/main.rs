ff621d8495896b2393b6fdd039b3dc22226b2cd1
Robbe Haegeman <robbe.haegeman@ugent.be>
2026-06-08 15:32:52 +0200

    feat: add wasmtime-wasi-crypto dep to wasmtime-host

--- wasmtime-host/src/main.rs
+++ wasmtime-host/src/main.rs
@@ -2,16 +2,27 @@ use std::error::Error;
 use wasmtime::{Engine, Store, component::Component, component::Linker, error::Context};
 use wasmtime_wasi::p2::bindings::Command;
 use wasmtime_wasi::{ResourceTable, WasiCtx, WasiCtxBuilder, WasiCtxView, WasiView};
+use wasmtime_wasi_crypto::crypto::{WasiCryptoCtx, WasiCryptoView};
 
 struct HostState {
     table: ResourceTable,
     wasi_ctx: WasiCtx,
+    crypto_ctx: WasiCryptoCtx,
 }
 
 impl WasiView for HostState {
     fn ctx(&mut self) -> WasiCtxView<'_> {
         WasiCtxView {
             ctx: &mut self.wasi_ctx,
+            table: &mut self.table,
+        }
+    }
+}
+
+impl WasiCryptoView for HostState {
+    fn crypto(&mut self) -> wasmtime_wasi_crypto::crypto::WasiCryptoCtxView<'_> {
+        wasmtime_wasi_crypto::crypto::WasiCryptoCtxView {
+            ctx: &mut self.crypto_ctx,
             table: &mut self.table,
         }
     }
@@ -25,6 +36,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let engine = Engine::default();
     let mut linker: Linker<HostState> = Linker::new(&engine);
     wasmtime_wasi::p2::add_to_linker_async(&mut linker)?;
+    wasmtime_wasi_crypto::crypto::add_to_linker(&mut linker)?;
 
     // https://docs.wasmtime.dev/api/wasmtime_wasi/p2/bindings/struct.Command.html
     let mut wasi_ctx_builder = WasiCtxBuilder::new();
@@ -36,6 +48,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let state = HostState {
         table: ResourceTable::new(),
         wasi_ctx: wasi_ctx_builder.build(),
+        crypto_ctx: WasiCryptoCtx::default(),
     };
 
     let mut store = Store::new(&engine, state);
